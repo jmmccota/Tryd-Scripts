c155e38d42c12f1a9f6c9f004991c940549d9b5c
João <joao_marcos_cota@hotmail.com>
2019-11-05 17:40:14 -0300

    Ajuste de cores.

--- AgressivityVolumeIndicator.groovy
+++ AgressivityVolumeIndicator.groovy
@@ -1,5 +1,6 @@
-// Indicador: Indicador de volume de compras
-// Author: JMMCCota
+// @Title: Indicador: Indicador de volume de compras
+// @Author: JMMCCota
+// @Version: 0.1.0
 
 // Settings //
 def len = 100;
@@ -42,43 +43,35 @@ for (int i = 1; i < vol.size(); i++) {
 
 
 	if (res > red) {
-		// bar.setFill(255,0,0); // vermelho
-		// bar.setBorder(255,0,0);
 		if (cOpen > cClose) {
-            bar.setFill(196, 0, 0); // Vermelho
-            bar.setBorder(128, 0, 0); // Cinza
+            bar.setFill(196, 0, 0); 
+            bar.setBorder(196, 0, 0); 
         } else {
-            bar.setFill(0, 196, 0); // Verde
-            bar.setBorder(128, 128, 128); // Cinza
+            bar.setFill(0, 196, 0); 
+            bar.setBorder(0, 196, 0); 
         }
-	}else if (res > orange) {
-		// bar.setFill(255,128,0); // laranja
-		// bar.setBorder(255,128,0);
+	} else if (res > orange) {
 		if (cOpen > cClose) {
-            bar.setFill(255, 128, 0); // Vermelho
-            bar.setBorder(128, 0, 0); // Cinza
+            bar.setFill(255, 255, 255);
+            bar.setBorder(255, 128, 0);
         } else {
-            bar.setFill(149, 255, 0); // Vermelho
-            bar.setBorder(0, 128, 0); // Cinza
+            bar.setFill(255, 255, 0); 
+            bar.setBorder(149, 255, 0);
         }
-	}else if (res > yellow) { // amarelo
-		// bar.setFill(250,244,2);
-		// bar.setBorder(250,244,2);
-
-        if (cOpen > cClose) {
-            bar.setFill(250,244,2); // Vermelho
-            bar.setBorder(250, 155, 2); // Cinza
-        } else {
-            bar.setFill(149, 255, 0); // Vermelho
-            bar.setBorder(159, 250, 2); // Cinza
-        }	
-	}else if (res > white) { // branco
-		bar.setFill(255,255,255);
-		bar.setBorder(255,255,255);
+	// }else if (res > yellow) { // amarelo
+    //     if (cOpen > cClose) {
+    //         bar.setFill(250,244,2); // Vermelho
+    //         bar.setBorder(250, 155, 2); // Cinza
+    //     } else {
+    //         bar.setFill(149, 255, 0); // Vermelho
+    //         bar.setBorder(159, 250, 2); // Cinza
+    //     }	
+	// }else if (res > white) { // branco
+	// 	bar.setFill(255,255,255);
+	// 	bar.setBorder(255,255,255);
 		
-	}else {
-		bar.setFill(0,252,252); // Azul
-		bar.setBorder(0,252,252);
-		
+	} else {
+		bar.setFill(255,255,255); // Azul
+		bar.setBorder(255,255,255);		
 	}
 }
