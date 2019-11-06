eb8c0c33ef23faeb0f53e7110bad980f35bfcd31
João <joao_marcos_cota@hotmail.com>
2019-11-05 22:47:30 -0300

    Mudança de nomes

--- AgressivityVolumeIndicator.groovy
+++ AgressivityVolumeIndicator.groovy
@@ -4,10 +4,10 @@
 
 // Settings //
 def len = 100;
-def red = 2.5;
-def orange = 1.0;
-def yellow = 0.6;
-def white = 0.15;
+def constanteNegocios = 0.5;
+def altoVolumeNegocios = 5*constanteNegocios;
+def medioVolumeNegocios = 2*constanteNegocios;
+def baixoVolumeNegocios = 1*constanteNegocios;
 // End //
 
 def barras = BARS();
@@ -15,10 +15,28 @@ def barras = BARS();
 r = newLines();
 r.add( barras );
 
-def vol = volume();
-def mean = MA(vol,len,0);
+def volumeNegociosPorTempo = volume();
 
-for (int i = 1; i < vol.size(); i++) {
+/**
+ MA( final LineData source, int period, int maType ) 
+
+  Descrição  - Moving Average 
+  Retorno  - LineData 
+  Parâmetros:  
+     maType  - Tipo de média móvel que será utilizada no cálculo. 
+        0 - Simples 
+        1 - Exponencial 
+        2 - Ponderada 
+        3 - Exponencial dupla 
+        4 - Exponencial tripla 
+        5 - Triangular 
+        6 - Kaufman Adaptive 
+        7 - MESA Adaptive 
+        8 - Exponencial tripla (T3) 
+*/
+def mean = MA(volumeNegociosPorTempo,len,0);
+
+for (int i = 1; i < volumeNegociosPorTempo.size(); i++) {
 	def bar = barras.bar(i);
 	def res = 0;
 
@@ -38,26 +56,27 @@ for (int i = 1; i < vol.size(); i++) {
     def cClose = bar.getClose();
 
 	if (i>len) {
-		res = vol.value(i)/mean.value(i - len + 1);
+		res = volumeNegociosPorTempo.value(i)/mean.value(i - len + 1);
 	}
 
 
-	if (res > red) {
+	if (res > altoVolumeNegocios) {
 		if (cOpen > cClose) {
             bar.setFill(196, 0, 0); 
             bar.setBorder(196, 0, 0); 
         } else {
             bar.setFill(0, 196, 0); 
             bar.setBorder(0, 196, 0); 
-	} else if (res > orange) {
+        }
+	} else if (res > medioVolumeNegocios) {
 		if (cOpen > cClose) {
             bar.setFill(255, 255, 255);
             bar.setBorder(255, 128, 0);
-      //  } else {
-       //     bar.setFill(255, 255, 0); 
-      //      bar.setBorder(149, 255, 0);
-     //   }
-	// }else if (res > yellow) { // amarelo
+       } else {
+           bar.setFill(255, 255, 255); 
+           bar.setBorder(149, 255, 0);
+       }
+	// }else if (res > baixoVolumeNegocios) { // amarelo
     //     if (cOpen > cClose) {
     //         bar.setFill(250,244,2); // Vermelho
     //         bar.setBorder(250, 155, 2); // Cinza
@@ -65,9 +84,9 @@ for (int i = 1; i < vol.size(); i++) {
     //         bar.setFill(149, 255, 0); // Vermelho
     //         bar.setBorder(159, 250, 2); // Cinza
     //     }	
-	// }else if (res > white) { // branco
-	// 	bar.setFill(255,255,255);
-	// 	bar.setBorder(255,255,255);
+	// }else if (res > baixissimoVolumeNegocios) { // branco
+		// bar.setFill(255,255,255);
+		// bar.setBorder(255,255,255);
 		
 	} else {
 		bar.setFill(255,255,255); // Azul
