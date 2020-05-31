4ed7bf143a2c10c7d248bd3d4fc18ebb8470c2b5
João <joao_marcos_cota@hotmail.com>
2020-05-31 16:50:13 -0300

    Ajustes

--- BarByVolume.groovy
+++ BarByVolume.groovy
@@ -1,48 +1,41 @@
 // @Title: Indicador: Indicador de barras de volume de compras com calculo volume refinado
 // @Author: JMMCCota
-// @Version: 0.1.5
+// @Version: 0.1.6
 
-def len = 200;
-def orange = 4;
-
-def mLines = 0; // 0-Disable 1-Enable
+def mLines = 1; // 0-Disable 1-Enable
+def len = 27;
 // Fim Parametros de configuracao //
 
 def v  = newLineData();
-def l5 = newLineData(); //orange
+def l5 = newLineData();
 
 def vol = volume();
-def mean = MA(vol,len, 0);
-def std = STDDEV(vol,len);
+
+def ma = MA(vol, len, 0);
+
 
 for (int i=len;i<vol.size();i++) {
-	if (i>len) {
-		double res = ( (vol.value(i) - mean.value(i-len+1)) / (std.value(i-len)));
-
-		double vatual = vol.value(i); 
-		double matual = mean.value(i-len+1);
-		double desvp  = std.value(i-len);
-
-		double val=0.0;
-
-		if (mLines){
-			val=matual+desvp*orange;
-			l5.add(val);
-			l5.setColor(255,0,0);   
-		}
-		if (res > orange) {
-			v.setColor(255,128,0);
-		} else {
-			v.setColor(64,64,64);
-		}
-
-		v.add(vatual);
-		v.setType(3);
+	if (vol.value(i) > ma.value(i-len)) {
+		v.setColor(255,128,0);
+	} else {
+		v.setColor(64,64,64);
 	}
+
+	v.add(vol.value(i));
+	v.setType(3);
 }
+
 def result = newLines();
 result.add(v);
+
 if (mLines){
-	result.add(l5);
+  for (int i=len;i<vol.size();i++) {
+    if(ma.value(i) != null){
+      l5.add(ma.value(i));
+    }
+    l5.setColor(255,0,0);   
+  }
+  result.add(l5);
 }
+
 r = result;
